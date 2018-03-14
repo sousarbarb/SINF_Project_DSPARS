a34d4f07a4d6e0abb068dfb5db82304f7a167b3a
Ricardo Barbosa Sousa <rbs15rbs@gmail.com>
2018-03-14 16:28:42 +0000

    Library structure manipulation

---
+++ Libraries test/Structures manipulation/manipulation_mote_struct.c
@@ -0,0 +1,129 @@
+/*
+ * manipulation_mote_struct.c
+ * 
+ * Copyright 2018 Ricardo Barbosa Sousa <sousa@debian-rbsousa>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include "manipulation_mote_struct.h"
+
+mote ** mote_vector_creation(int number_motes, int * error_func){
+	// Argument error checking
+	if(1 > number_motes){
+		printf("[ERROR_LIB_MAN_MOTE_STRUCT %d] mote_vector_creation: Number of motes invalid.\n", ERROR_LIB_MAN_MOTE_STRUCT_1);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_1;
+		return NULL;
+	}
+	
+	
+	// Function's code
+	int counter;
+	mote **system_motes = NULL;
+	system_motes = (mote **) malloc(sizeof(mote *) * number_motes);
+	if(NULL == system_motes){
+		printf("[ERROR_LIB_MAN_MOTE_STRUCT %d] mote_vector_creation: Memory allocated failed.\n", ERROR_LIB_MAN_MOTE_STRUCT_2);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_2;
+		return NULL;
+	}
+	for(counter = 0; counter < number_motes; counter++){
+		system_motes[counter] = (mote *) malloc(sizeof(mote));
+		if(NULL == system_motes[counter]){
+			printf("[ERROR_LIB_MAN_MOTE_STRUCT %d] mote_vector_creation: Memory allocated failed.\n", ERROR_LIB_MAN_MOTE_STRUCT_2);
+			free_mote_memory(system_motes, counter, NULL);
+			if(NULL != error_func)
+				(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_2;
+			return NULL;
+		}
+		system_motes[counter]->id = 0;
+		system_motes[counter]->luminosity = 0;
+		system_motes[counter]->temperature = 0;
+		system_motes[counter]->humidity = 0;
+	}
+	if(NULL != error_func)
+		(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_NONE;
+	return system_motes;
+}
+
+void free_mote_memory(mote** system_motes, int number_motes, int * error_func){
+	// Argument error checking
+	if(NULL == system_motes){
+		printf("[ERROR_LIB_MAN_MOTE_STRUCT %d] free_mote_memory: Pointer to the mote's structure invalid.\n", ERROR_LIB_MAN_MOTE_STRUCT_3);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_3;
+		return;
+	}
+	else if(0 > number_motes){
+		printf("[ERROR_LIB_MAN_MOTE_STRUCT %d] free_mote_memory: Number of motes invalid.\n", ERROR_LIB_MAN_MOTE_STRUCT_1);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_1;
+		return;
+	}
+	
+	
+	// Function's code
+	int counter;
+	for(counter = 0; counter < number_motes; counter++){
+		free(system_motes[counter]);
+		system_motes[counter] = NULL;
+	}
+	free(system_motes);
+	system_motes = NULL;
+	if(NULL != error_func)
+		(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_NONE;
+}
+
+int search_mote(mote** system_motes, int number_motes, int mote_id, int * error_func){
+	// Argument error checking
+	if(NULL == system_motes){
+		printf("[ERROR_LIB_MAN_MOTE_STRUCT %d] search_mote: Pointer to the mote's structure invalid.\n", ERROR_LIB_MAN_MOTE_STRUCT_3);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_3;
+		return -2;
+	}
+	else if(1 > number_motes){
+		printf("[ERROR_LIB_MAN_MOTE_STRUCT %d] search_mote: Number of motes invalid.\n", ERROR_LIB_MAN_MOTE_STRUCT_1);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_1;
+		return -2;
+	}
+	else if(1 > mote_id){
+		printf("[ERROR_LIB_MAN_MOTE_STRUCT %d] search_mote: Mote_id invalid invalid.\n", ERROR_LIB_MAN_MOTE_STRUCT_4);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_4;
+		return -2;
+	}
+	
+	
+	// Function's code
+	int counter;
+	for(counter = 0; (mote_id != system_motes[counter]->id) && (number_motes > counter); counter++);
+	if(number_motes == counter){
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_NONE;
+		return -1;
+	}
+	else{
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_MOTE_STRUCT_NONE;
+		return counter;
+	}
+}
