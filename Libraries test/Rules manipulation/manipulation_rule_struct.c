9afd1816db3e96eaf08eae54a8e83d38a4a725fb
Ricardo Barbosa Sousa <rbs15rbs@gmail.com>
2018-03-17 00:28:49 +0000

    upd5_17mar2018

--- Libraries test/Rules manipulation/manipulation_rule_struct.c
+++ Libraries test/Rules manipulation/manipulation_rule_struct.c
@@ -63,8 +63,10 @@ char **actuator_future_state_vector_creation(int number_actfutstat, int max_char
 		vector_act[counter] = (char *) malloc(max_charact_string);
 		if(NULL == vector_act[counter]){
 			printf("[ERROR_LIB_MAN_RULE_STRUCT %d] vector_actuator_future_state_creation: Memory allocated failed.\n", ERROR_LIB_MAN_RULE_STRUCT_3);
-			if(0 < counter)
-				free_actuator_future_state_memory(vector_act, counter, NULL);
+			if(0 < counter){
+				free(vector_act);
+				vector_act = NULL;
+			}
 			else{
 				free(vector_act);
 				vector_act = NULL;
@@ -78,34 +80,6 @@ char **actuator_future_state_vector_creation(int number_actfutstat, int max_char
 	if(NULL != error_func)
 		(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_NONE;
 	return vector_act;
-}
-
-void free_actuator_future_state_memory(char **vector_act, int number_actfutstat, int * error_func){
-	// Argument error checking
-	if(NULL == vector_act){
-		printf("[ERROR_LIB_MAN_RULE_STRUCT %d] free_actuator_future_state_memory: Pointer to the string's vector invalid.\n", ERROR_LIB_MAN_RULE_STRUCT_4);
-		if(NULL != error_func)
-			(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_4;
-		return;
-	}
-	else if(1 > number_actfutstat){
-		printf("[ERROR_LIB_MAN_RULE_STRUCT %d] free_actuator_future_state_memory: Number of future actuator state condition invalid.\n", ERROR_LIB_MAN_RULE_STRUCT_1);
-		if(NULL != error_func)
-			(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_1;
-		return;
-	}
-	
-	
-	// Function's code
-	int counter;
-	for(counter = 0; counter < number_actfutstat; counter++){
-		free(vector_act[counter]);
-		vector_act[counter] = NULL;
-	}
-	free(vector_act);
-	vector_act = NULL;
-	if(NULL != error_func)
-		(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_NONE;
 }
 
 void print_actuator_future_state_memory_vector(char **vector_act, int number_actfutstat, int * error_func){
@@ -126,9 +100,9 @@ void print_actuator_future_state_memory_vector(char **vector_act, int number_act
 	
 	// Function's code
 	int counter;
-	printf("\n+++++ ACTUATOR'S FUTURE STATE VECTOR +++++\n");
+	printf("\n    +++++ ACTUATOR'S FUTURE STATE VECTOR +++++\n");
 	for(counter = 0; counter < number_actfutstat; counter++)
-		printf("[%d] %s\n", counter, vector_act[counter]);
+		printf("    [%d] %s\n", counter, vector_act[counter]);
 	printf("\n");
 	if(NULL != error_func)
 		(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_NONE;
@@ -257,10 +231,10 @@ rule **rules_system_vector_creation(int number_rules, int * error_func){
 		
 		error_return = 0;
 		
-		strcpy(system_rules[counter]->logic_operator_condition_1_2, "___");
+		strcpy(system_rules[counter]->logic_operator_condition_1_2, lib_man_rule_logic_operators[0]);
 		system_rules[counter]->actuator_future_state = NULL;
-		system_rules[counter]->operator_condition_1  = '_';
-		system_rules[counter]->operator_condition_2  = '_';
+		system_rules[counter]->operator_condition_1  = comparison_operators[0];
+		system_rules[counter]->operator_condition_2  = comparison_operators[0];
 		system_rules[counter]->num_actuator_conditions = 0;
 		system_rules[counter]->value_condition_1 = 0;
 		system_rules[counter]->value_condition_2 = 0;
@@ -268,37 +242,6 @@ rule **rules_system_vector_creation(int number_rules, int * error_func){
 	if(NULL != error_func)
 		(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_NONE;
 	return system_rules;
-}
-
-void free_single_rule_memory(rule *system_rule, int * error_func){
-	// Argument error checking
-	if(NULL == system_rule){
-		printf("[ERROR_LIB_MAN_RULE_STRUCT %d] free_rules_system_memory: Pointer to rule structure invalid.\n", ERROR_LIB_MAN_RULE_STRUCT_7);
-		if(NULL != error_func)
-			(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_7;
-		return;
-	}
-	
-	
-	// Function's code
-	if(NULL != system_rule->division_name){
-		free(system_rule->division_name);
-		system_rule->division_name = NULL;
-	}
-	if(NULL != system_rule->sensor_condition_1){
-		free(system_rule->sensor_condition_1);
-		system_rule->sensor_condition_1 = NULL;
-	}
-	if(NULL != system_rule->sensor_condition_2){
-		free(system_rule->sensor_condition_2);
-		system_rule->sensor_condition_1 = NULL;
-	}
-	if(NULL != system_rule->actuator_future_state){
-		free_actuator_future_state_memory(system_rule->actuator_future_state, system_rule->num_actuator_conditions, NULL);
-		system_rule->actuator_future_state = NULL;
-	}
-	if(NULL != error_func)
-		(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_NONE;
 }
 
 void free_rules_system_memory(rule **system_rules, int number_rules, int * error_func){
@@ -319,7 +262,64 @@ void free_rules_system_memory(rule **system_rules, int number_rules, int * error
 	
 	// Function's code
 	int counter;
-	for(counter = 0; counter < number_rules; counter++)
-		free_single_rule_memory(system_rules[counter], NULL);
+	for(counter = 0; counter < number_rules; counter++){
+		if(NULL != system_rules[counter]->division_name){
+			free(system_rules[counter]->division_name);
+			system_rules[counter]->division_name = NULL;
+		}
+		if(NULL != system_rules[counter]->sensor_condition_1){
+			free(system_rules[counter]->sensor_condition_1);
+			system_rules[counter]->sensor_condition_1 = NULL;
+		}
+		if(NULL != system_rules[counter]->sensor_condition_2){
+			free(system_rules[counter]->sensor_condition_2);
+			system_rules[counter]->sensor_condition_1 = NULL;
+		}
+		if(NULL != system_rules[counter]->actuator_future_state){
+			free(system_rules[counter]->actuator_future_state);
+			system_rules[counter]->actuator_future_state = NULL;
+		}
+		free(system_rules[counter]);
+		system_rules[counter] = NULL;
+	}
+	printf("XXX\n");
+	free(system_rules);
+	system_rules = NULL;
+	printf("XXX\n");
+	if(NULL != error_func)
+		(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_NONE;
+}
+
+void print_rules_system_vector(rule **system_rules, int number_rules, int * error_func){
+	// Argument error checking
+	if(NULL == system_rules){
+		printf("[ERROR_LIB_MAN_RULE_STRUCT %d] print_rules_system_vector: Pointer to rule's system vector invalid.\n", ERROR_LIB_MAN_RULE_STRUCT_6);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_6;
+		return;
+	}
+	else if(1 > number_rules){
+		printf("[ERROR_LIB_MAN_RULE_STRUCT %d] print_rules_system_vector: Number of future actuator state condition invalid.\n", ERROR_LIB_MAN_RULE_STRUCT_1);
+		if(NULL != error_func)
+			(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_1;
+		return;
+	}
+	
+	
+	// Function's code
+	int counter;
+	printf("\n+++++ DIVISION RULES +++++\n");
+	for(counter = 0; counter < number_rules; counter++){
+		printf("[----- DIVISION %s -----]\n", system_rules[counter]->division_name);
+		if(comparison_operators[0] != system_rules[counter]->operator_condition_1)
+			printf("    Condition 1: %s %c %.2f\n", system_rules[counter]->sensor_condition_1, system_rules[counter]->operator_condition_1, system_rules[counter]->value_condition_1);
+		if(comparison_operators[0] != system_rules[counter]->operator_condition_2)
+			printf("    Condition 2: %s %c %.2f\n", system_rules[counter]->sensor_condition_2, system_rules[counter]->operator_condition_2, system_rules[counter]->value_condition_2);
+		if(NULL != system_rules[counter]->actuator_future_state && 0 < system_rules[counter]->num_actuator_conditions)
+			print_actuator_future_state_memory_vector(system_rules[counter]->actuator_future_state, system_rules[counter]->num_actuator_conditions,NULL);
+	}
+	printf("\n");
+	if(NULL != error_func)
+		(*error_func) = ERROR_LIB_MAN_RULE_STRUCT_NONE;
 }
 
