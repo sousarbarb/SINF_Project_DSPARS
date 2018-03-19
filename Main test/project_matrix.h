8fd83d8bb9a90ac8899e6bd123af810dd520a79f
Pedro Albergaria <up201504635@fe.up.pt>
2018-03-19 23:47:19 +0000

    Update

--- Main test/project_matrix.h
+++ Main test/project_matrix.h
@@ -1,7 +1,10 @@
 /*
  * define_size_matrix.c
  * 
- * Copyright 2018 Daniel Silva <daniel@debian>
+ * Copyright 2018
+ *     Daniel Queirós da Silva		<up201503071@fe.up.pt>
+ *     Pedro de Castro Albergaria	<up201504635@fe.up.pt>
+ *     Ricardo Barbosa Sousa		<up201503004@fe.up.pt>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
